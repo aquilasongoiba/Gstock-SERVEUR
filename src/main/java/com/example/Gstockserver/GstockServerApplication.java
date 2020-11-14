4e52e5cc7d49f0fcee2b85529e166528207abb94
aquilasongoiba <aquilasongoiba@gmail.com>
2020-11-14 18:07:01 +0000

    fisrt release

---
+++ src/main/java/com/example/Gstockserver/GstockServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gstockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstockServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gstockserver/GstockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gstockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
